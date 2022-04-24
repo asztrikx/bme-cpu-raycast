f55a53fc8455f28f1a8f67620c99fff1c6e35844
asztrikx <asztrik35@gmail.com>
2022-04-24 20:49:12 +0200

    remove comment

--- Skeleton.cpp
+++ Skeleton.cpp
@@ -1,40 +1,26 @@
 #include "framework.h"
-#include <bits/stdc++.h>
 
 struct Material {
-	// visszaverző képesség (3 hullámhosszon): ambiens, diffúz, spekuláris
 	vec3 ka, kd, ks;
-	// shine érték
 	float shininess;
-	// TODO miért *M_PI???XDD
 	Material(vec3 kd, vec3 ks, float shininess): ka(kd*M_PI), kd(kd), ks(ks), shininess(shininess) {}
 };
-
-// sugár és felület leírása: ezek kellenek róla
 struct Hit {
 	float t;
-	// világ koo-k
 	vec3 position, normal;
-	// Felületnél nem feltétlen homogén, lásd textúrázás
 	Material* material;
 	Hit() { t = -1; }
 };
-
-// sugár: félegyenes
 struct Ray {
 	vec3 start, dir;
-	// dir normalizálás
 	Ray(vec3 start, vec3 dir): start(start), dir(normalize(dir)) {}
 };
-
-//base class
 class Intersectable {
   protected:
 	Material* material;
 
 	Intersectable(Material* material): material(material) {}
   public:
-	// t==-1 => nincs találat
 	virtual Hit intersect(Ray const& ray) = 0;
 };
 
@@ -42,8 +28,7 @@ class Sphere: public Intersectable {
 	vec3 center;
 	float radius;
   public:
-	// TODO center why ref
-    Sphere(vec3& center, float radius, Material* material): Intersectable(material), center(center), radius(radius) {}
+    Sphere(vec3 center, float radius, Material* material): Intersectable(material), center(center), radius(radius) {}
 
 	Hit intersect(Ray const& ray) {
 		Hit hit;
@@ -52,88 +37,71 @@ class Sphere: public Intersectable {
 		float b = dot(ray.dir, dist) * 2.0f;
 		float c = dot(dist, dist) - radius * radius;
 		float discr = b*b-4.0f*a*c;
-		// no intersection
-		if (discr < 0) return hit; // default value is -1
+		if (discr < 0) return hit;
 		float sqrtDiscr = sqrtf(discr);
-		// t1 >= t2
 		float t1 = (-b+sqrtDiscr)/2.0f/a;
 		float t2 = (-b-sqrtDiscr)/2.0f/a;
-		// t1 behind camera => t2 also
 		if (t1 <= 0) return hit;
-		// by default t2 is closer to camera;
-		hit.t = t2>0 ? t2 : t1;
-
-		// t alapján visszahelyettesítés
+		hit.t = (t2>0) ? t2 : t1;
 		hit.position = ray.start + ray.dir * hit.t;
-		hit.normal = (hit.position - center) / radius;
+		hit.normal = (hit.position - center) * (1.0f / radius);
 		hit.material = material;
 		return hit;
 	}
 };
 
 class Camera {
-	// lookat: animáció mit nézzen
 	vec3 eye, lookat, right, up;
-	// "Computed value", kamera animációhoz kell
 	float fov;
   public:
-    // kamera beállítása: animáció miatt kell frissíteni a right és upot !!!!!!
-	//	viewUp: preferált függőleges irány
-	//	fov: kamera függőleges látószöge (négyzet => vízszintes is)
 	void set(vec3 eye, vec3 lookat, vec3 viewUp, float fov) {
 		this->eye = eye; this->lookat = lookat; this->fov = fov;
-		
-		// ortogonalizációs lépés: viewUp megkötés miatt
 		vec3 w = eye - lookat;
 		float windowSize = length(w) * tanf(fov / 2);
-		right = normalize(cross(viewUp,w)*windowSize);
+		right = normalize(cross(viewUp,w))*windowSize;
 		up = normalize(cross(w, right)) * windowSize;
 	}
 
 	Ray getRay(int X, int Y) {
-		// 0.5f: center of a pixel
-		float ndcx = 2*(X+0.5f)/windowWidth-1;
-		float ndcy = 2*(Y+0.5f)/windowHeight-1;
-		// eye to view panel ndc coos
+		float ndcx = 2.0f*(X+0.5f)/windowWidth-1;
+		float ndcy = 2.0f*(Y+0.5f)/windowHeight-1;
 		vec3 dir = (lookat + right*ndcx + up*ndcy) - eye;
 		return Ray(eye, dir);
 	}
-
-	// lookat ponton átmenő függőleges egyenes körül forgatás
-	// tekintet folyamastosan lookat ponton
 	void Animate(float dt) {
-		// lookat a pivot point !!!!!!!
 		vec3 d = eye - lookat;
-		// TODO Rotation matrix
-		eye = vec3(d.x * cosf(dt) + d.z * sinf(dt), d.y, -d.x * sin(dt) + d.z * cos(dt));
-		// pivot point visszatolás !!!!
+		vec4 tmp = vec4(d.x, d.y, d.z, 1);
+		vec4 eye4 = tmp * RotationMatrix(dt, vec3(0,0,1));
+		eye = vec3(eye4.x, eye4.y, eye4.z) / eye4.w;
 		eye = eye + lookat;
 		set(eye, lookat, up, fov);
 	}
 };
-
-// irányfényforrás
 struct Light {
 	vec3 direction;
 	vec3 Le; // intezitás: spektruma (3 hullámhossz)
-	Light(vec3 direction, vec3 Le): Le(Le) { this->Le = normalize(Le); } //!!!!
+	Light(vec3 direction, vec3 Le): direction(normalize(direction)), Le(Le) {} //!!!!
 };
 
 float rnd() { return (float)rand() / RAND_MAX; }
 float const eps = 0.0001f;
 
+void print(std::string text, vec3 v) {
+	printf("%s: %ef %ef %ef\n", text.c_str(), v.x, v.y, v.z);
+}
+
 class Scene {
 	std::vector<Intersectable*> objects;
 	std::vector<Light*> lights;
 	Camera camera;
-	// ambiens fény
 	vec3 La;
+
+	vec3 viewUp = vec3(-1,0,0);
+	vec3 lookat = vec3(0,0,-1);
+	vec3 eye = vec3(2,0,0);
+	float fov = 45 * M_PI / 180;
   public:
-	// virtuális világ felépítése: elemek elhelyezése
-	// TODO kísérletezés
 	void build() {
-		vec3 eye = vec3(0,0,2), viewUp = vec3(0,1,0), lookat = vec3(0,0,0);
-		float fov = 45 * M_PI / 180;
 		camera.set(eye,lookat,viewUp,fov);
 
 		La = vec3(0.4f, 0.4f, 0.4f);
@@ -144,16 +112,13 @@ class Scene {
 		Material* material1 = new Material(kd1, ks, 50);
 		Material* material2 = new Material(kd2, ks, 50);
 
-		for (int i = 0; i < 150; i++) {
-			// TODO más eltolás is jó?
+		for (int i = 0; i < 10; i++) {
 			vec3 pos1 = vec3(rnd()-0.5f, rnd()-0.5f, rnd()-0.5f);
 			vec3 pos2 = vec3(rnd()-0.5f, rnd()-0.5f, rnd()-0.5f);
 			objects.push_back(new Sphere(pos1, rnd()*0.1f, material1));
 			objects.push_back(new Sphere(pos2, rnd()*0.1f, material2));
 		}
 	}
-
-	// lefényképezés
 	void render(std::vector<vec4>& image) {
 		long timeStart = glutGet(GLUT_ELAPSED_TIME);
 
@@ -164,18 +129,14 @@ class Scene {
 				image[Y * windowWidth + X] = vec4(color.x, color.y, color.z, 1);
 			}
 		}
-		
-		printf("Rtime %d ms\n",glutGet(GLUT_ELAPSED_TIME)-timeStart);
 	}
 
 	Hit firstIntersect(Ray ray) {
 		Hit bestHit;
 		for (Intersectable* object: objects) {
 			Hit hit = object->intersect(ray);
-			// eddig még nem volt valid min eset kezelése
 			if(hit.t > 0 && (bestHit.t < 0 || hit.t < bestHit.t)) bestHit = hit;
 		}
-		// normál vektor felénk mutasson !!!!!!!!!!!!!
 		if (dot(ray.dir, bestHit.normal) > 0) bestHit.normal = bestHit.normal * (-1);
 		return bestHit;
 	}
@@ -192,27 +153,33 @@ class Scene {
 		if(hit.t < 0) return La;
 		vec3 outRadiance = hit.material->ka * La;
 		for (Light* light: lights) {
-			// fényforrás látható-e a szemből látható pontban!!
-			// TODO kísérlet: törlés vagy eps nélkül
-			// normál vektor kifele mutat
-			// light->direction: fény fele mutat
-			Ray shadowRay(hit.position + hit.normal * eps, light->direction);
+			Ray shadowRay(hit.position + hit.normal * eps, light->direction); // eps!!!
 			float cosTheta = dot(hit.normal, light->direction); // egységvektorok !!!!
-			// cos negatív: ez az obj árnyékol maga
-			// más obj takar-e
-			// shadowIntersect: csak a visszatérés miatt új fgv
 			if (cosTheta > 0 && !shadowIntersect(shadowRay)) {
 				outRadiance = outRadiance + light->Le * hit.material->kd * cosTheta;
-				// V = -ray.dir !!!!
-				// egységvektorok !!!!
 				vec3 halfway = normalize(-ray.dir + light->direction);
 				float cosDelta = dot(hit.normal, halfway); //egységvektorok !!!!
-				// TODO ??
-				//std::assert(cosDelta > 0);
+				if (cosDelta <= 0) printf("----%ef\n", cosDelta);
 				if (cosDelta > 0) outRadiance = outRadiance + light->Le*hit.material->ks*powf(cosDelta, hit.material->shininess);
 			}
 		}
 		return outRadiance;
+	}
+
+	void translate(float dt) {
+		print("viewUp",viewUp);
+		vec4 viewUp4 = vec4(viewUp.x,viewUp.y,viewUp.z);
+		viewUp4 = viewUp4 * RotationMatrix(dt, vec3(0,1,0));
+		viewUp = vec3(viewUp4.x, viewUp4.y, viewUp4.z);
+		print("viewUp",viewUp);
+
+		print("lookat",lookat);
+		vec4 lookat4 = vec4(lookat.x,lookat.y,lookat.z);
+		lookat4 = lookat4 * RotationMatrix(dt, vec3(0,1,0));
+		lookat = vec3(lookat4.x, lookat4.y, lookat4.z);
+		print("lookat",lookat);
+		
+		camera.set(eye,lookat,viewUp,fov);
 	}
 
 	void Animate(float dt) { camera.Animate(dt); }
@@ -229,7 +196,7 @@ const char * const vertexSource = R"(
 	out vec2 uv;
 
 	void main() {
-		uv = (cVP + vec(1,1)) / 2;
+		uv = (cVP + vec2(1,1)) / 2;
 		gl_Position = vec4(cVP.xy, 0, 1);
 	}
 )";
@@ -246,8 +213,6 @@ const char * const fragmentSource = R"(
 		outColor = texture(textureUnit, uv);
 	}
 )";
-
-// téglalap: ndc-beli teljesen kitöltve, textúrázás is
 class FullScreenTexturedQuad {
 	unsigned int vao = 0, textureId = 0;
   public:
@@ -263,11 +228,8 @@ class FullScreenTexturedQuad {
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vpCoos), vpCoos, GL_STATIC_DRAW);
 		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,0,nullptr);
-
-		// EGÉSZ!!!!
 		glGenTextures(1, &textureId);
 		glBindTexture(GL_TEXTURE_2D, textureId);
-		// TODO miért kell 2? honnan tudja mikor van min és mag
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); //bilineáris szűrés
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	}
@@ -282,9 +244,7 @@ class FullScreenTexturedQuad {
 		int location = glGetUniformLocation(gpuProgram.getId(), "textureUnit");
 		const unsigned int textureUnit = 0;
 		if (location >= 0) {
-			// uniform beáll
 			glUniform1i(location, textureUnit);
-			// textúra mintavevő egység akitválása
 			glActiveTexture(GL_TEXTURE0 + textureUnit);
 			glBindTexture(GL_TEXTURE_2D, textureId);
 		}
@@ -308,45 +268,29 @@ void onDisplay() {
 	fullScreenTexturedQuad->LoadTexture(image);
 	fullScreenTexturedQuad->Draw();
 	glutSwapBuffers();
-
-	//glClearColor(0, 0, 0, 0);
-	//glClear(GL_COLOR_BUFFER_BIT);
 }
 
 void onKeyboard(unsigned char key, int pX, int pY) {
-	if (key == 'd') glutPostRedisplay();
+	return;
+	switch (key) {
+	case 't': scene.translate(0.1f); break;
+	case 'g': scene.translate(-0.1f); break;
+	}
+	
+	glutPostRedisplay();
 }
 
 void onKeyboardUp(unsigned char key, int pX, int pY) {
 }
 
 void onMouseMotion(int pX, int pY) {
-
-	float cX = 2.0f * pX / windowWidth - 1;
-	float cY = 1.0f - 2.0f * pY / windowHeight;
-	printf("Mouse moved to (%3.2f, %3.2f)\n", cX, cY);
 }
 
 void onMouse(int button, int state, int pX, int pY) {
-
-	float cX = 2.0f * pX / windowWidth - 1;
-	float cY = 1.0f - 2.0f * pY / windowHeight;
-
-	char * buttonStat;
-	switch (state) {
-	case GLUT_DOWN: buttonStat = "pressed"; break;
-	case GLUT_UP:   buttonStat = "released"; break;
-	}
-
-	switch (button) {
-	case GLUT_LEFT_BUTTON:   printf("Left button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);   break;
-	case GLUT_MIDDLE_BUTTON: printf("Middle button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY); break;
-	case GLUT_RIGHT_BUTTON:  printf("Right button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);  break;
-	}
 }
 
 void onIdle() {
+	return;
 	scene.Animate(0.1f);
 	glutPostRedisplay(); //!!!!
-	//long time = glutGet(GLUT_ELAPSED_TIME);
 }
