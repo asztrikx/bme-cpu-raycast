977216e2756c346d98159eeeb96a76d610ae5ba8
asztrikx <asztrik35@gmail.com>
2022-04-25 12:23:12 +0200

    add Plane, Cylinder

--- Skeleton.cpp
+++ Skeleton.cpp
@@ -1,20 +1,28 @@
 #include "framework.h"
 
+float floatEqual(float subject, float number) {
+	float eps = 0.001;
+	return subject > number - eps && subject < number + eps;
+}
+
 struct Material {
 	vec3 ka, kd, ks;
 	float shininess;
 	Material(vec3 kd, vec3 ks, float shininess): ka(kd*M_PI), kd(kd), ks(ks), shininess(shininess) {}
 };
+
 struct Hit {
 	float t;
 	vec3 position, normal;
 	Material* material;
 	Hit() { t = -1; }
 };
+
 struct Ray {
 	vec3 start, dir;
 	Ray(vec3 start, vec3 dir): start(start), dir(normalize(dir)) {}
 };
+
 class Intersectable {
   protected:
 	Material* material;
@@ -23,6 +31,16 @@ class Intersectable {
   public:
 	virtual Hit intersect(Ray const& ray) = 0;
 };
+
+float quardraticSmaller(float a, float b, float c) {
+	float discr = b*b-4.0f*a*c;
+	if (discr < 0) return nanf("");
+	float sqrtDiscr = sqrtf(discr);
+	float t1 = (-b+sqrtDiscr)/2.0f/a;
+	float t2 = (-b-sqrtDiscr)/2.0f/a;
+	if (t1 <= 0) return nanf("");
+	return (t2>0) ? t2 : t1;
+}
 
 class Sphere: public Intersectable {
 	vec3 center;
@@ -36,16 +54,86 @@ class Sphere: public Intersectable {
 		float a = dot(ray.dir, ray.dir);
 		float b = dot(ray.dir, dist) * 2.0f;
 		float c = dot(dist, dist) - radius * radius;
-		float discr = b*b-4.0f*a*c;
-		if (discr < 0) return hit;
-		float sqrtDiscr = sqrtf(discr);
-		float t1 = (-b+sqrtDiscr)/2.0f/a;
-		float t2 = (-b-sqrtDiscr)/2.0f/a;
-		if (t1 <= 0) return hit;
-		hit.t = (t2>0) ? t2 : t1;
+
+		float sol = quardraticSmaller(a,b,c);
+		if (isnan(sol)) return hit;
+
+		hit.t = sol;
 		hit.position = ray.start + ray.dir * hit.t;
-		hit.normal = (hit.position - center) * (1.0f / radius);
+		hit.normal = (hit.position - center) / radius;
 		hit.material = material;
+		return hit;
+	}
+};
+
+struct Plane: public Intersectable {
+	vec3 normal;
+	vec3 point;
+	
+	Plane(vec3 normal, vec3 point, Material* material): Intersectable(material), normal(normalize(normal)), point(point) {}
+
+	Hit intersect(Ray const& ray) {
+		Hit hit;
+		float divisor = dot(ray.dir, normal);
+		if (floatEqual(divisor, 0)) return hit;
+		float t = - dot(ray.start, normal) / divisor;
+		if (t < 0) return hit;
+		hit.position = ray.start + t*ray.dir;
+		hit.normal = normal;
+		hit.material = material;
+		hit.t = t;
+		return hit;
+	}
+};
+
+class Cylinder: public Intersectable {
+	vec3 start;
+	float height;
+	vec3 dir;
+	vec3 normal;
+	float r;
+
+	std::vector<Plane> planes;
+
+  public:
+	Cylinder(vec3 start, float height, float r, vec3 dir, Material* material): Intersectable(material), start(start), height(height), r(r), dir(normalize(dir)) {
+		normal.x = 1;
+		normal.y = 0;
+		normal.z = -dir.x / dir.z;
+		normal = normalize(normal);
+
+		planes.push_back(Plane(dir, start, material));
+		planes.push_back(Plane(dir, start + dir * height, material));
+	}
+
+	Hit intersect(Ray const& ray) {
+		Hit hit;
+
+		for (Plane plane: planes) {
+			Hit planeHit = plane.intersect(ray);
+			if (length(planeHit.position - plane.point) <= r) {
+				return planeHit;
+			}
+		}
+
+		// TODO use multiplication for simpler form
+		float a = powf(normal.x * ray.dir.x, 2) + powf(normal.y * ray.dir.y, 2);
+		float b = 2*normal.x*ray.dir.x*(ray.start.x - start.x) + 2*normal.y*ray.dir.y*(ray.start.y - start.y);
+		float c = powf(normal.x*(ray.start.x-start.x), 2) + powf(normal.y*(ray.start.y-start.y), 2) - powf(r,2);
+
+		float sol = quardraticSmaller(a,b,c);
+		if (isnan(sol)) return hit;
+
+		vec3 position = ray.start + ray.dir * hit.t;
+		float distanceFromStart = dot(dir, position - start);
+		if (distanceFromStart > height || distanceFromStart < 0) return hit;
+
+		vec3 center = start + distanceFromStart*dir;
+
+		hit.t = sol;
+		hit.material = material;
+		hit.normal = (position - center) / r;
+		hit.position = position;
 		return hit;
 	}
 };
@@ -68,19 +156,20 @@ class Camera {
 		vec3 dir = (lookat + right*ndcx + up*ndcy) - eye;
 		return Ray(eye, dir);
 	}
+
 	void Animate(float dt) {
 		vec3 d = eye - lookat;
 		vec4 tmp = vec4(d.x, d.y, d.z, 1);
 		vec4 eye4 = tmp * RotationMatrix(dt, vec3(0,0,1));
-		eye = vec3(eye4.x, eye4.y, eye4.z) / eye4.w;
+		eye = vec3(eye4.x, eye4.y, eye4.z);
 		eye = eye + lookat;
 		set(eye, lookat, up, fov);
 	}
 };
 struct Light {
 	vec3 direction;
-	vec3 Le; // intezitás: spektruma (3 hullámhossz)
-	Light(vec3 direction, vec3 Le): direction(normalize(direction)), Le(Le) {} //!!!!
+	vec3 Le;
+	Light(vec3 direction, vec3 Le): direction(normalize(direction)), Le(Le) {}
 };
 
 float rnd() { return (float)rand() / RAND_MAX; }
@@ -96,10 +185,10 @@ class Scene {
 	Camera camera;
 	vec3 La;
 
-	vec3 viewUp = vec3(-1,0,0);
-	vec3 lookat = vec3(0,0,-1);
-	vec3 eye = vec3(2,0,0);
-	float fov = 45 * M_PI / 180;
+	vec3 viewUp = vec3(0,0,1);
+	vec3 lookat = vec3(0,0,0);
+	vec3 eye = vec3(5,0,0.8f);
+	float fov = 45.0f / 180 * M_PI;
   public:
 	void build() {
 		camera.set(eye,lookat,viewUp,fov);
@@ -109,21 +198,19 @@ class Scene {
 		lights.push_back(new Light(lightDirection, Le));
 
 		vec3 kd1(0.3f, 0.2f, 0.1f), kd2(0.1f, 0.2f, 0.3f), ks(2,2,2);
-		Material* material1 = new Material(kd1, ks, 50);
-		Material* material2 = new Material(kd2, ks, 50);
+		Material* materialPlane = new Material(kd1, ks, 50);
+		Material* materialLamp = new Material(kd2, ks, 50);
 
-		for (int i = 0; i < 10; i++) {
-			vec3 pos1 = vec3(rnd()-0.5f, rnd()-0.5f, rnd()-0.5f);
-			vec3 pos2 = vec3(rnd()-0.5f, rnd()-0.5f, rnd()-0.5f);
-			objects.push_back(new Sphere(pos1, rnd()*0.1f, material1));
-			objects.push_back(new Sphere(pos2, rnd()*0.1f, material2));
-		}
+		objects.push_back(new Plane(vec3(0,0,1), vec3(0,0,0), materialPlane));
+		objects.push_back(new Sphere(vec3(0,0,0), 0.1f, materialLamp));
+		objects.push_back(new Cylinder(vec3(0,0,0), 10, 1, vec3(0,0,1), materialLamp));
 	}
+	
 	void render(std::vector<vec4>& image) {
 		long timeStart = glutGet(GLUT_ELAPSED_TIME);
 
+		// TODO pragma
 		for (int Y = 0; Y < windowHeight; Y++) {
-#pragma omp parallel for
 			for (int X = 0; X < windowWidth; X++) {
 				vec3 color = trace(camera.getRay(X, Y));
 				image[Y * windowWidth + X] = vec4(color.x, color.y, color.z, 1);
@@ -148,17 +235,17 @@ class Scene {
 		return false;
 	}
 
-	vec3 trace(Ray ray, int depth = 0) {
+	vec3 trace(Ray ray) {
 		Hit hit = firstIntersect(ray);
 		if(hit.t < 0) return La;
 		vec3 outRadiance = hit.material->ka * La;
 		for (Light* light: lights) {
-			Ray shadowRay(hit.position + hit.normal * eps, light->direction); // eps!!!
-			float cosTheta = dot(hit.normal, light->direction); // egységvektorok !!!!
+			Ray shadowRay(hit.position + hit.normal * eps, light->direction);
+			float cosTheta = dot(hit.normal, light->direction);
 			if (cosTheta > 0 && !shadowIntersect(shadowRay)) {
 				outRadiance = outRadiance + light->Le * hit.material->kd * cosTheta;
 				vec3 halfway = normalize(-ray.dir + light->direction);
-				float cosDelta = dot(hit.normal, halfway); //egységvektorok !!!!
+				float cosDelta = dot(hit.normal, halfway);
 				if (cosDelta <= 0) printf("----%ef\n", cosDelta);
 				if (cosDelta > 0) outRadiance = outRadiance + light->Le*hit.material->ks*powf(cosDelta, hit.material->shininess);
 			}
@@ -166,23 +253,7 @@ class Scene {
 		return outRadiance;
 	}
 
-	void translate(float dt) {
-		print("viewUp",viewUp);
-		vec4 viewUp4 = vec4(viewUp.x,viewUp.y,viewUp.z);
-		viewUp4 = viewUp4 * RotationMatrix(dt, vec3(0,1,0));
-		viewUp = vec3(viewUp4.x, viewUp4.y, viewUp4.z);
-		print("viewUp",viewUp);
-
-		print("lookat",lookat);
-		vec4 lookat4 = vec4(lookat.x,lookat.y,lookat.z);
-		lookat4 = lookat4 * RotationMatrix(dt, vec3(0,1,0));
-		lookat = vec3(lookat4.x, lookat4.y, lookat4.z);
-		print("lookat",lookat);
-		
-		camera.set(eye,lookat,viewUp,fov);
-	}
-
-	void Animate(float dt) { camera.Animate(dt); }
+	void Animate(float dt) { /*camera.Animate(dt);*/  eye.z += 1; camera.set(eye,lookat,viewUp,fov); }
 };
 
 Scene scene;
@@ -213,6 +284,7 @@ const char * const fragmentSource = R"(
 		outColor = texture(textureUnit, uv);
 	}
 )";
+
 class FullScreenTexturedQuad {
 	unsigned int vao = 0, textureId = 0;
   public:
@@ -271,13 +343,9 @@ void onDisplay() {
 }
 
 void onKeyboard(unsigned char key, int pX, int pY) {
-	return;
 	switch (key) {
-	case 't': scene.translate(0.1f); break;
-	case 'g': scene.translate(-0.1f); break;
+	case 'd': glutPostRedisplay(); break;
 	}
-	
-	glutPostRedisplay();
 }
 
 void onKeyboardUp(unsigned char key, int pX, int pY) {
@@ -290,7 +358,6 @@ void onMouse(int button, int state, int pX, int pY) {
 }
 
 void onIdle() {
-	return;
 	scene.Animate(0.1f);
-	glutPostRedisplay(); //!!!!
+	glutPostRedisplay();
 }
